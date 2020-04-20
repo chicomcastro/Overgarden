bd6cca3321d9bb06860fd07a0d05e93cb484279f
edmsimp <63891357+edmsimp@users.noreply.github.com>
2020-04-20 17:52:57 -0300

    Score Display

---
+++ Overgarden/Assets/ScoreScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : DataHolder
+{
+    public Text points;
+
+    private void Update()
+    { 
+        points.text = DataHolder.instance.GetScore().ToString(); 
+    }
+}
